72e1c332ee67ce53ef30ebe890869df2484935f3
xxx <46323989+Fork3xxx@users.noreply.github.com>
2020-10-28 11:13:27 +0800

    添加了happyIceFire项目

---
+++ MyProject/happyIceFire/src/ui/Character.java
@@ -0,0 +1,21 @@
+package ui;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * 游戏场景中的每种角色
+ */
+public abstract class Character
+{
+    //图片
+    BufferedImage img;
+    //坐标
+    int x;
+    int y;
+    //宽高
+    static  int w;
+    int h;
+
+
+
+}
